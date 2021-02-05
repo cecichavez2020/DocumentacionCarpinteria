83636883199c613b9ad760b7b1942b5347ce47c2
cecichavez2020 <57550447+cecichavez2020@users.noreply.github.com>
2021-02-05 01:17:57 -0500

    Add files via upload

---
+++ MaderaXpress/inc/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <img src="assets/icons/social-facebok.png" alt="facebook-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Google +">
+            <img src="assets/icons/social-google.png" alt="googleplus-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Whatsapp">
+            <img src="assets/icons/social-whasat.png" alt="linkedin-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Messenger">
+            <img src="assets/icons/social-messenger.png" alt="pinterest-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <img src="assets/icons/social-twiteer.png" alt="twitter-icon">
+        </a>
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">MaderaXpress &copy; 2021</h5>
+</footer>
---
+++ MaderaXpress/library/configServer.php
@@ -0,0 +1,5 @@
+<?php
+define("USER", "id16053937_root");
+define("SERVER", "localhost");
+define("BD", "id16053937_store");
+define("PASS", "");
